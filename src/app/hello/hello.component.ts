f54e1cc6d31a682f0e8f29033f46fe110e738f79
Abdullah Numan <anewman15@hotmail.com>
2021-08-13 23:33:19 +0600

    Add css style to component metadata

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -3,7 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'hello',
   templateUrl: './hello.component.html',
-  styleUrls: ['./hello.component.scss']
+  styleUrls: [`
+    .text-success {
+      color: green;
+    }
+
+    .text-danager {
+      color: red;
+    }
+
+    .text-special {
+      font-style: italic;
+    }
+  `]
 })
 export class HelloComponent implements OnInit {
   public name = 'Abdullah'
