ddb6d51e7ba8f4736f573649721fe133880cafd7
Abdullah Numan <anewman15@hotmail.com>
2021-09-03 17:47:04 +0600

    Define getEmployees method in employee.service

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -4,6 +4,25 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class EmployeeService {
-
   constructor() { }
+
+  getEmployees() {
+    return [
+      {
+        'id': 1,
+        'firstName': 'Abdullah',
+        'lastName': 'Numan'
+      },
+      {
+        'id': 1,
+        'firstName': 'Abdullah',
+        'lastName': 'Mamun'
+      },
+      {
+        'id': 1,
+        'firstName': 'Sahera',
+        'lastName': 'Begum'
+      }
+    ]
+  }
 }
