04f55319b2f5d2ddced2bb474f166b38238fd5ca
Abdullah Numan <anewman15@hotmail.com>
2021-09-03 17:37:01 +0600

    Generate EmployeeList component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello/hello.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloComponent
+    HelloComponent,
+    EmployeeListComponent
   ],
   imports: [
     BrowserModule,
