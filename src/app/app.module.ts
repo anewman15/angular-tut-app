cd12c8974f6af1dad5e8daccf574149666335bc2
Abdullah Numan <anewman15@hotmail.com>
2021-09-03 17:50:41 +0600

    Register EmployeeService in app.module in providers array

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello/hello.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { EmployeeService } from './employee.service';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,7 @@ import { EmployeeListComponent } from './employee-list/employee-list.component';
     AppRoutingModule,
     FormsModule
   ],
-  providers: [],
+  providers: [EmployeeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
