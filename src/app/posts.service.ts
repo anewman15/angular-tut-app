74273e0797cbe1ff3867371b335fc908820f39c0
Abdullah Numan <anewman15@hotmail.com>
2021-09-06 22:56:36 +0600

    Define getPosts() method, cast Observable into array of Ipost interface objects

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,10 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IPost } from './posts-list/post';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
-
+  private _postsUrl: string = 'https://jsonplaceholder.typicode.com/posts'
   constructor(private http: HttpClient) { }
+
+  getPosts(): Observable<IPost[]> {
+    return this.http.get<IPost[]>(this._postsUrl);
+  }
 }
