a0a6bd609c515f3dcdd4c0f19394beecea3ef4d8
Abdullah Numan <anewman15@hotmail.com>
2021-09-06 23:05:32 +0600

    Pass PostsService to PostsListComponent, subscribe and save data in ngOninit

--- src/app/posts-list/posts-list.component.ts
+++ src/app/posts-list/posts-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PostsService } from '../posts.service';
+import { IPost } from './post';
 
 @Component({
   selector: 'app-posts-list',
@@ -6,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./posts-list.component.scss']
 })
 export class PostsListComponent implements OnInit {
+  public posts: IPost[] = [];
 
-  constructor() { }
+  constructor(private _postsService: PostsService) { }
 
   ngOnInit(): void {
+    this._postsService.getPosts()
+      .subscribe(data => this.posts = data);
   }
 
 }
